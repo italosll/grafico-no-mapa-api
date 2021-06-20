bbee366e07b3001e60ba38943e69ff1e59623e9d
italosll <italomsilva.if@gmail.com>
2021-06-20 10:05:26 -0300

    ➕ Add CORS

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import express from 'express';
 import routes from './routes';
 
 // var express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
