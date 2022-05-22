cb301f6bd62754cd525d57802cb888536bee3d0b
italosll <italomsilva.if@gmail.com>
2022-05-22 16:28:38 -0300

    👷 updated build

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,5 +1,3 @@
 import { createConnection, Connection } from 'typeorm';
-import { Answer } from '../entities/Answer';
-import { Profile } from '../entities/Profile';
 
-const connection = await createConnection({ entities: [Answer, Profile] });
+const connection = await createConnection();
