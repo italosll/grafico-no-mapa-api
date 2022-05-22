2b1fe1435a4f08f4310b1caa292af3415258b524
italosll <italomsilva.if@gmail.com>
2022-05-22 11:14:39 -0300

    🔧 encryot config

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,3 +1,3 @@
-import { createConnection, Connection } from "typeorm";
+import { createConnection, Connection } from 'typeorm';
 
-const connection = await createConnection();
+const connection = await createConnection({ options: { encrypt: false } });
