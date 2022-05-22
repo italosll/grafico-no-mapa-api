0347fe0598816c53abfcde2d67e8dabcb7bdaf9f
italosll <italomsilva.if@gmail.com>
2022-05-22 16:08:22 -0300

    ♻️ update entities declaration

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,3 +1,3 @@
 import { createConnection, Connection } from 'typeorm';
 
-const connection = await createConnection({ entities: [path.join(process.cwd(), './src/entities/*.ts,.js')] });
+const connection = await createConnection({ entities: [path.join(process.cwd(), './src/entities/*{.ts,.js}')] });
